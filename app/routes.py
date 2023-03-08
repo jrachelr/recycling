046d31e187c0c7397ec4ae3cc7777e3872544c02
ayeryn <erynli@utexas.edu>
2023-03-08 13:47:16 -0600

    Add layout and home html with bootstrap 5

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,43 @@
 from app import app
+from flask import redirect, render_template
 
 
 @app.route("/")
 @app.route("/home")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('home.html', title='Home')
+
+
+@app.route("/about")
+def about():
+    return 'Aboyut'
+
+# TODO: User specific routes
+
+
+@app.route("/register")
+def register():
+    return 'register'
+
+
+@app.route("/login")
+def login():
+    return 'Login'
+
+
+@app.route("/logout")
+def logout():
+    return 'Logout'
+
+
+@app.route("/account")
+def account():
+    return 'account'
+
+# TODO: Location specific routes
+    return 'About'
+
+
+@app.route("/locations")
+def locations():
+    return 'locations'
