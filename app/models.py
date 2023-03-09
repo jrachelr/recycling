0b5ae0c902f5c3fbd5a2e03eab1db82edd9587e7
ayeryn <erynli@utexas.edu>
2023-03-09 17:25:13 -0600

    Update Location model

--- app/models.py
+++ app/models.py
@@ -2,14 +2,34 @@ from app import db
 
 # TODO: complete Location model
 
+# saved_locations = db.Table('saved_locations',
+#                            db.Column('user_id', db.Integer, db.ForeignKey('User.id')),
+#                            db.Column('location_id', db.Integer, db.ForeignKey('Location.id'))
+#                            )
+
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
-    address = db.Column(db.String(20), unique=True, nullable=False)
+    street_address = db.Column(db.String(40), nullable=False)
+    city = db.Column(db.String(20), nullable=False)
+    state = db.Column(db.String(20), nullable=False)
+
+    # accepted_items = db.relationship('AcceptedItem', backref='')
 
     def __repr__(self) -> str:
-        return f'Location(name={self.name}, address={self.address})'
+        return f'Location({self.name}, {self.street_address}, {self.city}, {self.state})'
 
-# TODO:
-# class User(db.Model):
+
+# class AcceptedItem(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+    # location_list =
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    # saved_locations = db.relationship('Location', secondary=saved_locations)
