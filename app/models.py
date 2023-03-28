347f3f54e87d29985fa9a3cf713daeb17f8bf629
rachel <jrachelr1@gmail.com>
2023-03-28 15:21:50 -0700

    last change

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from flask import url_for
 
 
 saved_locations = db.Table(
@@ -41,6 +42,29 @@ class Location(db.Model):
         for field in ["name", "street_address", "city", "state"]:
             if field in data:
                 setattr(self, field, data[field])
+
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page=page, per_page=per_page, error_out=False)
+        data = {
+            "items": [item.to_dict() for item in resources.items],
+            "_meta": {
+                "page": page,
+                "per_page": per_page,
+                "total_pages": resources.pages,
+                "total_items": resources.total,
+            },
+            "_links": {
+                "self": url_for(endpoint, page=page, per_page=per_page, **kwargs),
+                "next": url_for(endpoint, page=page + 1, per_page=per_page, **kwargs)
+                if resources.has_next
+                else None,
+                "prev": url_for(endpoint, page=page - 1, per_page=per_page, **kwargs)
+                if resources.has_prev
+                else None,
+            },
+        }
+        return data
 
 
 class Item(db.Model):
